a5238fecc8a038c6c520302eaa3131738f73ca3e
Johans <johansneirap@gmail.com>
2021-08-18 00:42:24 -0400

    update skills section

--- src/components/ui/Leftsidebar.js
+++ src/components/ui/Leftsidebar.js
@@ -7,10 +7,10 @@ export const Leftsidebar = () => {
               <div className="left-sidebar ">
                   <div className="icon-bar make-me-sticky animate__animated animate__fadeInLeft animate__slower">
                 <ul className="list-group ">
-                    <li className="grow"><a href="https://github.com/johansneirap" target="_hblank" className="github"><i className="fa fa-github"></i></a></li>
-                    <li className="grow"><a href="https://www.facebook.com/profile.php?id=100008287421153" target="_hblank" className="facebook"><i className="fa fa-facebook"></i></a></li>
-                    <li className="grow"><a href="https://www.instagram.com/joh4nsennn/" target="_hblank" className="instagram"><i className="fa fa-instagram"></i></a></li>
-                    <li className="grow"><a href="https://youtube.com" target="_hblank" className="youtube"><i className="fa fa-youtube"></i></a></li>
+                    <li className="grow"><a href="https://github.com/johansneirap" target="_hblank" className="github"><i className="fab fa-github"></i></a></li>
+                    <li className="grow"><a href="https://www.facebook.com/profile.php?id=100008287421153" target="_hblank" className="facebook"><i className="fab fa-facebook"></i></a></li>
+                    <li className="grow"><a href="https://www.instagram.com/joh4nsennn/" target="_hblank" className="instagram"><i className="fab fa-instagram"></i></a></li>
+                    <li className="grow"><a href="https://www.linkedin.com/in/johans-neira/" target="_hblank" className="linkedin"><i className="fab fa-linkedin"></i></a></li>
                     <br></br>
                 </ul>
               </div>
--- src/components/ui/SectionSkills.js
+++ src/components/ui/SectionSkills.js
@@ -5,7 +5,42 @@ export const SectionSkills = () => {
   return (
     <div className="panel-section section3" id="skills">
       <h1 data-splitting className="bigh1 section-title"> Habilidades</h1>
-      <p>Work in progress...</p>
+      
+      <div className="container">
+        <div className="skill-box">
+         <i className="fab fa-react"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-js"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-vuejs"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-html5"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-sass"></i>
+        </div>
+      </div>
+      <div className="container">
+        
+      <div className="skill-box">
+         <i className="fab fa-node"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-npm"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-python"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-aws"></i>
+        </div>
+        <div className="skill-box">
+         <i className="fab fa-git-alt"></i>
+        </div>
+      </div>
     </div>
   );
 };
